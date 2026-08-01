d38293d4e181026b68a6466b7e8d2c75df16287e
Aiman Meteora <aimaniszreen@gmail.com>
2026-08-01 21:18:44 +0800

    Delete zclaw-console directory

--- zclaw-console/build/bootloader/CMakeFiles/bootloader.elf.dir/project_elf_src_esp32s3.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/bootloader.elf.dir/project_elf_src_esp32s3.c.obj: \
- /home/meteora/esp/esp-idf/zclaw-console/build/bootloader/project_elf_src_esp32s3.c
--- zclaw-console/build/esp-idf/zclaw/CMakeFiles/__idf_zclaw.dir/main/telegram_poll_policy.c.obj.d
+++
@@ -1,5 +0,0 @@
-esp-idf/zclaw/CMakeFiles/__idf_zclaw.dir/main/telegram_poll_policy.c.obj: \
- /home/meteora/esp/esp-idf/zclaw-console/components/zclaw/main/telegram_poll_policy.c \
- /home/meteora/esp/esp-idf/zclaw-console/components/zclaw/main/telegram_poll_policy.h \
- /home/meteora/.espressif/tools/xtensa-esp-elf/esp-14.2.0_20241119/xtensa-esp-elf/lib/gcc/xtensa-esp-elf/14.2.0/include/stdbool.h \
- /home/meteora/esp/esp-idf/zclaw-console/components/zclaw/main/config.h
