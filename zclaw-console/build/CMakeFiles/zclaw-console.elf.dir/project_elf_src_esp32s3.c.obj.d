f47bc008ed8f7a67734dd09bfb72886b7e63a378
AimanMeteora <aimaniszreen@gmail.com>
2026-08-01 21:36:13 +0800

    Initial project upload

---
+++ zclaw-console/build/CMakeFiles/zclaw-console.elf.dir/project_elf_src_esp32s3.c.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/zclaw-console.elf.dir/project_elf_src_esp32s3.c.obj: \
+ /home/meteora/esp/esp-idf/zclaw-console/build/project_elf_src_esp32s3.c
---
+++ zclaw-console/build/esp-idf/espcoredump/CMakeFiles/__idf_espcoredump.dir/src/core_dump_crc.c.obj.d
@@ -0,0 +1,3 @@
+esp-idf/espcoredump/CMakeFiles/__idf_espcoredump.dir/src/core_dump_crc.c.obj: \
+ /home/meteora/esp/esp-idf/components/espcoredump/src/core_dump_crc.c \
+ /home/meteora/esp/esp-idf/zclaw-console/build/config/sdkconfig.h
